d829181db63a4bcb7bb4f775bdbebf350e698bc7
calebjanhunen <34351243+calebjanhunen@users.noreply.github.com>
2018-01-23 16:28:39 -0500

    Camera moves up and platforms respawn at top of screen

--- core/src/gdx/game/ScrPlay.java
+++ core/src/gdx/game/ScrPlay.java
@@ -10,12 +10,15 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import java.util.ArrayList;
 import java.util.Random;
+
 /**
  *
  * @author chaud2180
@@ -29,10 +32,11 @@ public class ScrPlay implements Screen, InputProcessor {
     Rectangle rectDino, rectPlatform;
     Sprite sprDino, sprPlatform;
     ShapeRenderer shapeRenderer;
+    public OrthographicCamera camera;
     int nYDinoX = 100, nYDinoY = 200, nYDinoWidth = 75, nYDinoHeight = 100, nSpriteSpeed = 5, nPlatWidth = 200, nPlatHeight = 50, nCountJump = 0, nCountOverlap = 0;
-    int arnPlatform[] = new int[5];
+    int arnPlatform[] = new int[5], arnTempPlatform[] = new int[5];
     boolean bCanFall = true, bCanJump = true;
-    double dGravity = 0.5, dFallSpeed = 0, dJumpSpeed = 20;
+    double dGravity = 0.5, dFallSpeed = 0, dJumpSpeed = 17;
 
     public ScrPlay(Game game) {
         game = game;
@@ -43,6 +47,9 @@ public class ScrPlay implements Screen, InputProcessor {
         sprDino = new Sprite(txtdino);
         sprPlatform = new Sprite(txtplatform);
         main = new ScrMenu();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false);
+        camera.position.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
         shapeRenderer = new ShapeRenderer();
         arnPlatform = CreatePlatforms();
     }
@@ -87,7 +94,6 @@ public class ScrPlay implements Screen, InputProcessor {
         sprDino.setSize(nYDinoWidth, nYDinoHeight);
         sprDino.setPosition(nYDinoX, nYDinoY);
         rectDino = new Rectangle(sprDino.getX(), sprDino.getY(), sprDino.getWidth(), sprDino.getHeight());
-
         for (int i = 1; i < arnPlatform.length; i++) {
             int nPlatformX = arnPlatform[i];
             int nPlatformY = 200 * i;
@@ -101,6 +107,20 @@ public class ScrPlay implements Screen, InputProcessor {
                 bCanJump = true;
                 bCanFall = false;
                 dFallSpeed = 0;
+            }
+            while (rectPlatform.getY() <= camera.position.y - (Gdx.graphics.getHeight() / 2)) {
+                nPlatformX = arnPlatform[i];
+                nPlatformY = nPlatformY + 1000;
+                sprPlatform.setSize(nPlatWidth, nPlatHeight);
+                sprPlatform.setPosition(nPlatformX, nPlatformY);
+                rectPlatform = new Rectangle(sprPlatform.getX(), sprPlatform.getY(), sprPlatform.getWidth(), sprPlatform.getHeight());
+                arnRectPlatform[i] = rectPlatform;
+                isOverlapping = rectDino.overlaps(arnRectPlatform[i]);
+                if (isOverlapping) {
+                    bCanJump = true;
+                    bCanFall = false;
+                    dFallSpeed = 0;
+                }
             }
         }
     }
@@ -122,10 +142,13 @@ public class ScrPlay implements Screen, InputProcessor {
             nCountJump++;
             nYDinoY += dJumpSpeed;
             dJumpSpeed -= dGravity;
-            if (nCountJump >= 30) {
+            if (nYDinoY >= camera.position.y) {
+                camera.position.y += dJumpSpeed;
+            }
+            if (nCountJump >= 35) {
                 bCanJump = false;
                 bCanFall = true;
-                dJumpSpeed = 20;
+                dJumpSpeed = 17;
                 nCountJump = 0;
             }
         }
@@ -133,22 +156,32 @@ public class ScrPlay implements Screen, InputProcessor {
 
     @Override
     public void show() {
-        return;
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
         batch.begin();
-        batch.draw(txtbackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //background
+        batch.draw(txtbackground, 0, camera.position.y - 500, 600, 1000); //background
         batch.draw(txtdino, nYDinoX, nYDinoY, nYDinoWidth, nYDinoHeight); //yellow dino
         for (int i = 1; i < arnPlatform.length; i++) { // platforms
             int nPlatformX = arnPlatform[i];
-            int nPlatformY = 200 * i;
-            batch.draw(txtplatform, nPlatformX, nPlatformY, nPlatWidth, nPlatHeight);
+            int nPlatformY = 250 * i;
+            batch.draw(sprPlatform, nPlatformX, nPlatformY, nPlatWidth, nPlatHeight);
+            while (nPlatformY <= camera.position.y - 500) {
+                nPlatformX = arnPlatform[i];
+                nPlatformY = nPlatformY + 1000;
+                batch.draw(sprPlatform, nPlatformX, nPlatformY, nPlatWidth, nPlatHeight);
+            }
         }
         batch.end();
-
+        //while (nPlatformY <= camera.position.y - 500) {
+        //}
+        if (nYDinoY <= camera.position.y - 500) {
+            System.out.println("Game Over");
+        }
+        batch.setProjectionMatrix(camera.combined);
         HandleKeys();
         ScreenWrap();
         HitDetection();
@@ -157,8 +190,7 @@ public class ScrPlay implements Screen, InputProcessor {
     }
 
     @Override
-    public void resize(int width, int height
-    ) {
+    public void resize(int width, int height) {
         return;
     }
 
@@ -183,50 +215,42 @@ public class ScrPlay implements Screen, InputProcessor {
     }
 
     @Override
-    public boolean keyDown(int keycode
-    ) {
+    public boolean keyDown(int keycode) {
         return false;
     }
 
     @Override
-    public boolean keyUp(int keycode
-    ) {
+    public boolean keyUp(int keycode) {
         return false;
     }
 
     @Override
-    public boolean keyTyped(char character
-    ) {
+    public boolean keyTyped(char character) {
         return false;
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button
-    ) {
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         return false;
     }
 
     @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button
-    ) {
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         return false;
     }
 
     @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer
-    ) {
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
 
     @Override
-    public boolean mouseMoved(int screenX, int screenY
-    ) {
+    public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
 
     @Override
-    public boolean scrolled(int amount
-    ) {
+    public boolean scrolled(int amount) {
         return false;
     }
 }
